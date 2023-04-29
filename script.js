9c46d94be71785c02ffbd503dc66cb3b7a07f720
Mohd Jamal <79620177+Mohdjamal1@users.noreply.github.com>
2023-04-29 21:52:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
 //your JS code here. If required.
+const now = new Date();
+let nowDate = document.getElementById("timer");
+nowDate.innerText = `${now.toLocaleDateString()}, ${now.toLocaleTimeString()}`;
